66ad521875c3c13a5dd72a10b472ec32969d73ba
Luis Guzman <luis.dev@lmaero.pro>
2021-12-26 08:53:40 -0500

    Adjust header styles

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,12 +1,12 @@
 import Typed from './../../node_modules/typed.js/src/typed.js';
 
 const options = {
-  backDelay: 1000,
+  backDelay: 2000,
   backSpeed: 30,
   loop: true,
   smartBackspace: true,
   strings: ['fessional', 'active', 'ficient', 'ductive'],
-  typeSpeed: 60,
+  typeSpeed: 70,
 };
 
 const typed = new Typed('.typed', options);
